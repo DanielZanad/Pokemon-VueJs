ab7e72c37d626d8b9607d2af003887e87e635e51
Zanad <daniel.zanad.dz@gmail.com>
2021-05-08 13:14:50 -0300

    Adicionando base da aplicação
    Fazendo o request na api e monstrando as informações

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, index) in pokemons" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Pokemon from './components/Pokemon'
+export default {
+  name: 'App',
+  components:{
+    Pokemon
+  },
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      console.log("Pegou a lista de pokmons");
+      this.pokemons = res.data.results;
+    })
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemon.front" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">          
+          <div class="media-content">
+            <p class="title is-4">{{num}} - {{name | upper}}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+  
+        </div>
+      </div>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created: function(){
+    axios.get(this.url).then(res =>{
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+      console.log(this.pokemon)
+    })
+  },
+  data(){
+    return{
+      pokemon:{
+        type: '',
+        front: '',
+        back: ''
+      }
+    }
+  },
+  props:{
+      num: Number,
+      name: String,
+      url: String
+  },
+  filters:{
+    upper: function(value){
+      // Da para pegar as letras de uma string, como se fosse um array
+      var newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    }
+  }
+}
+</script>
+
+<style>
+  div#pokemon{
+    margin-top: 2%;
+  }
+</style>
