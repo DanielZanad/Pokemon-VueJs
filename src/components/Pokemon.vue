fbbf04a8efe92937536d0148086d6311db1ed93b
Zanad <daniel.zanad.dz@gmail.com>
2021-05-08 13:26:46 -0300

    Adicionando botão que muda o sprite do pokemon

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-image">
         <figure>
-          <img :src="pokemon.front" alt="Placeholder image">
+          <img :src="currentImg" alt="Placeholder image">
         </figure>
       </div>
       <div class="card-content">
@@ -15,7 +15,7 @@
         </div>
 
         <div class="content">
-  
+          <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar sprite</button>
         </div>
       </div>
     </div>
@@ -32,11 +32,13 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
-      console.log(this.pokemon)
+      this.currentImg = this.pokemon.front;
     })
   },
   data(){
     return{
+      isFront: true,
+      currentImg: '',
       pokemon:{
         type: '',
         front: '',
@@ -51,9 +53,19 @@ export default {
   },
   filters:{
     upper: function(value){
-      // Da para pegar as letras de uma string, como se fosse um array
       var newName = value[0].toUpperCase() + value.slice(1);
       return newName;
+    }
+  },
+  methods:{
+    mudarSprite: function(){
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front
+      }
     }
   }
 }
