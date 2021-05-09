7d981f740d6a2c03a6f3fce735e53a9438782e14
Zanad <daniel.zanad.dz@gmail.com>
2021-05-09 12:15:06 -0300

    Adicionando botão de busca

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <img src="./assets/pokemon-logo.png" alt="">
       <hr>
       <h4 class="is-size-4">Pokedex</h4>
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <input type="text" class="input is-rounded" placeholder="Buscar pokemon pelo nome" v-model="busca">
+      <button class="button is-fullwidth button is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
       </div>
     </div>
@@ -21,16 +23,28 @@ export default {
   },
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
       console.log("Pegou a lista de pokmons");
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons
+      if(this.busca == '' || this.busca == ''){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
   }
-  
 }
 </script>
 
@@ -42,5 +56,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
